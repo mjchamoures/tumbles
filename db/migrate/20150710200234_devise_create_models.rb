63910c1557198d2f4202c632b4873dc339278fa7
mjchamoures <mchamoures0@gmail.com>
2015-07-10 13:16:26 -0700

    implemented devise for users

--- db/migrate/20150710200234_devise_create_models.rb
+++ db/migrate/20150710200234_devise_create_models.rb
@@ -1,6 +1,6 @@
-class AddDeviseToModels < ActiveRecord::Migration
-  def self.up
-    change_table(:models) do |t|
+class DeviseCreateModels < ActiveRecord::Migration
+  def change
+    create_table(:models) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -31,19 +31,12 @@ class AddDeviseToModels < ActiveRecord::Migration
       # t.datetime :locked_at
 
 
-      # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps
+      t.timestamps
     end
 
     add_index :models, :email,                unique: true
     add_index :models, :reset_password_token, unique: true
     # add_index :models, :confirmation_token,   unique: true
     # add_index :models, :unlock_token,         unique: true
-  end
-
-  def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
   end
 end
